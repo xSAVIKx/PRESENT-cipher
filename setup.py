9e537fd3a358ced76c7991f852ff19d2a3ab5f65
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-05-12 22:26:11 +0300

    setup.py test file created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='present',
+    version='0.0.1',
+    packages=['present'],
+    url='https://github.com/xSAVIKx/present',
+    license='',
+    author='Iurii Sergiichuk',
+    author_email='iurii.sergiichuk@gmail.com',
+    description='etalon python implementation of PRESENT cipher and minified version with 16-bit key and 8-bit block'
+)
