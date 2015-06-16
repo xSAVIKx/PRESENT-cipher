ae7df7139c5b2d312eb5f0b95c0824581bfc2829
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2015-06-17 00:29:58 +0300

    update setup.py to v.0.0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='present',
-    version='0.0.1',
-    packages=['present', 'gmac'],
+    version='0.0.2',
+    packages=['present', 'gmac', 'gmac/util'],
     url='https://github.com/xSAVIKx/present',
-    license='',
+    license='Apache License, Version 2.0',
     author='Iurii Sergiichuk',
     author_email='iurii.sergiichuk@gmail.com',
-    description='etalon python implementation of PRESENT cipher and minified version with 16-bit key and 8-bit block'
+    description='python implementation of PRESENT cipher and minified version with 16-bit key and 8-bit block, implementation of GMAC using minified python algorithm as cipher'
 )
