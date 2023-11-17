6129d4e08e8f27e34730075b16a439daee70574e
fredtux <fredtux@users.noreply.github.com>
2023-11-17 20:39:18 +0200

    Revived project

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='present',
-    version='0.0.2',
+    version='0.0.3',
     packages=['present', 'gmac', 'gmac/util'],
     url='https://github.com/xSAVIKx/present',
     license='Apache License, Version 2.0',
-    author='Iurii Sergiichuk',
+    author='Iurii Sergiichuk, Florin Dinu',
     author_email='iurii.sergiichuk@gmail.com',
     description='python implementation of PRESENT cipher and minified version with 16-bit key and 8-bit block, implementation of GMAC using minified python algorithm as cipher'
 )
